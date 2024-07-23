8b1336f303a926b93a6137f45340ca95f1044e93
ILikeForest <namu90541@gmail.com>
2024-07-23 17:18:59 +0900

    조회와 템플릿 업데이트

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -1,8 +1,13 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+    questionList = Question.objects.order_by('-create_date')
+    context = {'questionList': questionList}
+    return render(request, 'pybo/questionList.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question' : question}
+    return render(request, 'pybo/questionDetail.html', context)
