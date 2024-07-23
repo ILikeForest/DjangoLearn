0512b1827c50b34fe94d30158e52cbd35296a525
ILikeForest <namu90541@gmail.com>
2024-07-23 17:00:06 +0900

    오류 계속 나던 것 admin까지 복구함

--- mysite/manage.py
+++ mysite/manage.py
@@ -3,20 +3,10 @@
 import os
 import sys
 
+
 def main():
     """Run administrative tasks."""
-    # 현재 파일의 디렉토리 경로를 가져옵니다.
-    current_path = os.path.dirname(os.path.abspath(__file__))
-    
-    # 프로젝트 폴더의 상대 경로로 이동합니다.
-    project_path = os.path.join(current_path, 'config')  # 'config'를 실제 프로젝트 이름으로 변경하세요.
-    
-    # sys.path에 프로젝트 폴더를 추가합니다.
-    sys.path.append(project_path)
-    
-    # Django 설정 모듈 환경 변수를 설정합니다.
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')  # 'config.settings'를 실제 설정 모듈로 변경하세요.
-    
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -27,6 +17,6 @@ def main():
         ) from exc
     execute_from_command_line(sys.argv)
 
+
 if __name__ == '__main__':
     main()
-    
--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from .models import Question
+
+# Create your views here.
+from django.http import HttpResponse
 
 
 def index(request):
-    question_list = Question.objects.order_by('-create_date')
-    context = {'question_list': question_list}
-    return render(request, 'pybo/questionList.html', context)
+    return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
