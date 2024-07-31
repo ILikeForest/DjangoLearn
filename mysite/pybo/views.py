a9b8b9540e1c6c557a87f6926544655b3982d906
ILikeForest <namu90541@gmail.com>
2024-07-31 13:34:31 +0900

    모델변경 - 글쓴이 표시 준비

--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, get_object_or_404,  redirect
 from django.utils import timezone
 from .models import Question, Answer
-from django.http import HttpResponseNotAllowed
 from .forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
 
 def index(request):
@@ -19,6 +19,7 @@ def detail(request, question_id):
     context = {'question' : question}
     return render(request, 'pybo/questionDetail.html', context)
 
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     '''
     pybo 답변등록
@@ -28,12 +29,13 @@ def answer_create(request, question_id):
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.User    # author 속성에 로그인 계정 저장
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
             return redirect('pybo:detail', question_id=question.id)
     else:
-        return HttpResponseNotAllowed('Only POST is possible.')
+        form = AnswerForm()
     context = {'question': question, 'form': form}
     return render(request, 'pybo/questionDetail.html', context)
     '''
@@ -43,11 +45,13 @@ def answer_create(request, question_id):
     return redirect('pybo:detail', question_id=question.id)
     '''
 
+@login_required(login_url='common:login')
 def question_create(request):
     if request.method == 'POST':
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)
+            question.author = request.user  # author 속성에 로그인 계정 저장
             question.create_date = timezone.now()
             question.save()
             return redirect('pybo:index')
